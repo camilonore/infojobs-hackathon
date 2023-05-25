e84ef664fb8c6183c658abf575466878de3556e3
Camilo Nore <caminore22@gmail.com>
2023-05-25 14:15:20 -0500

    Change from service to services

--- src/services/infojobsServices.ts
+++ src/services/infojobsServices.ts
@@ -6,27 +6,27 @@ import type {
   OfferResponse
 } from 'types/infojobsApiTypes'
 
-class InfojobsService {
+class InfojobsServices {
   private static headers = {
     Authorization: `Basic ${token}`
   }
 
   private static async fetchApi(url: string): Promise<Response> {
     return fetch(url, {
-      headers: InfojobsService.headers
+      headers: InfojobsServices.headers
     })
   }
 
   static async getRelevantJobs(): Promise<JobList> {
     const url =
       'https://api.infojobs.net/api/9/offer?order=relevancia-desc&maxResults=5'
-    const response = await InfojobsService.fetchApi(url)
+    const response = await InfojobsServices.fetchApi(url)
     return response.json()
   }
 
   static async getFacets(): Promise<Facet[]> {
     const url = 'https://api.infojobs.net/api/9/offer?facets=true&maxResults=1'
-    const response = await InfojobsService.fetchApi(url)
+    const response = await InfojobsServices.fetchApi(url)
     const data: OfferResponse = await response.json()
     const facets = data.facets
     return facets
@@ -34,15 +34,15 @@ class InfojobsService {
 
   static async getOfferDetails(offerId: string): Promise<OfferDetailsResponse> {
     const url = `https://api.infojobs.net/api/7/offer/${offerId}`
-    const response = await InfojobsService.fetchApi(url)
+    const response = await InfojobsServices.fetchApi(url)
     return response.json()
   }
 
   static async searchOffers(query: string): Promise<OfferResponse> {
     const url = `https://api.infojobs.net/api/9/offer?${query}`
-    const response = await InfojobsService.fetchApi(url)
+    const response = await InfojobsServices.fetchApi(url)
     return response.json()
   }
 }
 
-export { InfojobsService }
+export { InfojobsServices }
